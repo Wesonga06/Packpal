e4aa253821d0fbac5da8f104ccaa81031c8d9659
cindy <cindy@Wesonga>
2025-10-13 17:21:03 +0100

    initial commit

--- src/controllers/WelcomeController.java
+++ src/controllers/WelcomeController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package packpal.controllers;
+package controllers;
 
 import packpal.views.WelcomeView;
 import packpal.views.LoginView;
