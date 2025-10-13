2d70576ad22e8ba9ef942ef6cbbbf64c872458f6
cindy <cindy@Wesonga>
2025-10-13 20:59:50 +0300

    commit

--- src/models/PackingProgress.java
+++ src/models/PackingProgress.java
@@ -1,47 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
-/**
- *
- * @author cindy
- */
-class PackingProgress {
+public class PackingProgress {
     private int totalItems;
     private int packedItems;
-    
-    public PackingProgress(int totalItems, int PackedItems){
+
+    public PackingProgress(int totalItems, int packedItems) {
         this.totalItems = totalItems;
         this.packedItems = packedItems;
     }
-    
-    public int getTotalItems(){
+
+    public int getTotalItems() {
         return totalItems;
     }
-    
-    public void setTotalItems(int totalItems){
+
+    public void setTotalItems(int totalItems) {
         this.totalItems = totalItems;
     }
-    
-    public int getPackedItems(int PackedItems){
+
+    public int getPackedItems() {
         return packedItems;
     }
-    
-    public void  setPackedItems(int packedItems){
+
+    public void setPackedItems(int packedItems) {
         this.packedItems = packedItems;
     }
-    
-    public double getCompletionPercentage(){
-        if(totalItems == 0) return 0.0;
-        return((double) packedItems / totalItems) *100.0;
+
+    // 🟩 Calculate completion percentage safely
+    public double getCompletionPercentage() {
+        if (totalItems == 0) return 0.0;
+        return ((double) packedItems / totalItems) * 100.0;
     }
-    
-    
+
     @Override
-    public String toString(){
-        return String.format("Packed %d of %d items (%.2f%%)", packedItems, totalItems, getCompletionPercentage());
+    public String toString() {
+        return String.format("Packed %d of %d items (%.2f%%)", 
+            packedItems, totalItems, getCompletionPercentage());
     }
-    
 }
